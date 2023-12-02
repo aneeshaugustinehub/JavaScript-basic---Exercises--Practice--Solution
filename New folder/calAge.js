4d57fd781067a463fe959cf81c008c8e9fcbc1c1
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2023-12-02 22:29:57 +0530

    added age calulater

---
+++ New folder/calAge.js
@@ -0,0 +1,18 @@
+
+
+
+let yob = 1999;
+let mob = 6;
+
+function calulateAge(yob, mob) {
+  let date = new Date();
+
+  console.log(
+    date.getFullYear() - yob,
+    "year and",
+    date.getMonth() - mob,
+    "month"
+  );
+}
+
+calulateAge(yob, mob);
